a4854a1c3a5b96fcd321090a4913edf53a122d90
DMNK <plasku.dominik1217@gmail.com>
2024-11-05 23:47:25 +0100

    Updated Projekt - Created the home page
    - since default landing page is the welcome.blade.php I removed the default generated content and started working on the home page
    - created a navbar which will require some change in the future
    - created a footer and placed the team member's name on it for now as a placeholder
    - I had set up some base Tailwind CSSS to make a default look based on the login/registration page

---
+++ Projekt/webshop-app/resources/views/components/footer.blade.php
@@ -0,0 +1,10 @@
+<div class="bg-gray-950 backdrop-blur-lg bg-opacity-40 p-4 fixed bottom-0 w-full">
+    <div class="flex justify-center items-center space-x-2">
+        <ul class="flex space-x-10 text-gray-300">
+            <li>Borsodi István</li>
+            <li>Cserni Boglárka Anna</li>
+            <li>Nagy Péter</li>
+            <li>Plasku Dominik</li>
+        </ul>
+    </div>
+</div>
