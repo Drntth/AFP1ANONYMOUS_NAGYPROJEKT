7eb5122f9539e628f8547e6b398b74fc06a5d01a
BorsodiIstvan <borsodiistvan639@gmail.com>
2024-11-05 17:13:42 +0100

    Updated Projekt - create frontend for register, login, forgot password
    The frontend is ready for the following files:
    - forgot-password.blade.php
    - login.blade.php
    - register.blade.php

--- Projekt/webshop-app/resources/views/auth/login.blade.php
+++ Projekt/webshop-app/resources/views/auth/login.blade.php
@@ -7,16 +7,16 @@
 
         <!-- Email Address -->
         <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+            <x-input-label for="email" :value="__('Email')" class="text-white"/>
+            <x-text-input id="email" class="block mt-1 w-full focus:border-slate-600 focus:ring-slate-600 border-2" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+            <x-input-label for="password" :value="__('Password')" class="text-white"/>
 
-            <x-text-input id="password" class="block mt-1 w-full"
+            <x-text-input id="password" class="block mt-1 w-full focus:border-slate-600 focus:ring-slate-600 border-2"
                             type="password"
                             name="password"
                             required autocomplete="current-password" />
@@ -27,19 +27,19 @@
         <!-- Remember Me -->
         <div class="block mt-4">
             <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                <input id="remember_me" type="checkbox" class="rounded text-slate-600 shadow-sm focus:ring-0 focus:ring-slate-600 focus:outline-none" name="remember">
+                <span class="ms-2 text-sm text-white">{{ __('Remember me') }}</span>
             </label>
         </div>
 
-        <div class="flex items-center justify-end mt-4">
+        <div class="flex items-center justify-between mt-4">
             @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                <a class="underline text-sm text-white hover:text-slate-600 rounded-md focus:outline-none" href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
                 </a>
             @endif
 
-            <x-primary-button class="ms-3">
+            <x-primary-button class="ms-3 bg-slate-700 hover:bg-slate-800 focus:outline-none focus:ring-0 focus:ring-transparent ring-offset-transparent active:bg-slate-800">
                 {{ __('Log in') }}
             </x-primary-button>
         </div>
