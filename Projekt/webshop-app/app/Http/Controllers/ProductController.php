db1781b307851c7dd4744030572369242d6e64ad
Boglárka Cserni <csernibogi@gmail.com>
2024-11-05 21:00:16 +0100

    Updated Project - CRUD for product done
    I finished CRUD operations for product by adding a table for viewing database contents, and forms to update and add new product and a button to delete them

--- Projekt/webshop-app/app/Http/Controllers/ProductController.php
+++ Projekt/webshop-app/app/Http/Controllers/ProductController.php
@@ -9,7 +9,8 @@ class ProductController extends Controller
 {
     public function index()
     {
-        return view('products.index'); //itt lehet nem kell az s betű
+        $products = Product::all();
+        return view('products.index', ['products' => $products]); //itt lehet nem kell az s betű
     }
 
     public function add()
@@ -31,5 +32,31 @@ class ProductController extends Controller
         $newProduct = Product::create($data);
         return redirect(route('product.index'));
 
+    }
+
+    public function edit(Product $product) 
+    {
+        return view('products.edit', ['product' => $product]);
+    }
+
+    public function update(Product $product, Request $request) 
+    {
+        $data = $request->validate([
+           'name' => 'required',
+            'description' => 'nullable',
+            'price' => 'required|decimal:0,2',
+            'category_id' => 'required|numeric',
+            'image' => 'required',
+            'stock' => 'required|numeric', 
+        ]);
+
+        $product->update($data);
+        return redirect(route('product.index'))->with('success', 'Product Updated Successfully!');
+    }
+
+    public function destroy(Product $product) 
+    {
+        $product->delete();
+        return redirect(route('product.index'))->with('success', 'Product Deleted Successfully!');
     }
 }
--- Projekt/webshop-app/resources/views/products/index.blade.php
+++ Projekt/webshop-app/resources/views/products/index.blade.php
@@ -7,6 +7,49 @@
 </head>
 <body>
     <h1>Product</h1>
-    <div>index</div>
+    <div>
+        @if (session('success'))
+            <div></div>{{session('success')}}</div>
+        @endif
+    </div>
+    <div>
+        <a href="{{route('product.add')}}">Add a new Product</a>
+    </div>
+    <div>
+        <table border="1">
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Description</th>
+                <th>Price</th>
+                <th>Category ID</th>
+                <th>Stock</th>
+                <th>Image</th>
+                <th>Edit</th>
+                <th>Delete</th>
+            </tr>
+            @foreach ($products as $product)
+            <tr>
+                <td>{{$product->id}}</td>
+                <td>{{$product->name}}</td>
+                <td>{{$product->description}}</td>
+                <td>{{$product->price}}</td>
+                <td>{{$product->category_id}}</td>
+                <td>{{$product->stock}}</td>
+                <td>{{$product->image}}</td>
+                <td>
+                    <a href="{{route('product.edit', ['product' => $product])}}">Edit</a>
+                </td>
+                <td>
+                    <form method="post" action="{{route('product.destroy', ['product' => $product])}}">
+                        @csrf
+                        @method('delete')
+                        <input type="submit" value="Delete">
+                    </form>
+                </td>
+            </tr>
+            @endforeach
+        </table>
+    </div>
 </body>
 </html>
