d0f7db22c30ae20921533313fa6205a67a1596f4
DMNK <plasku.dominik1217@gmail.com>
2024-11-08 22:50:24 +0100

    Updated Projekt - Small Navbar Fix
    -  fix to 'dashboard/profile' elements overlapping the dropdown elements from navbar because of the blurr and overwriting the double stacking the background

--- Projekt/webshop-app/resources/views/profile/edit.blade.php
+++ Projekt/webshop-app/resources/views/profile/edit.blade.php
@@ -5,7 +5,7 @@
         </h2>
     </x-slot>
 
-    <div class="py-12 bg-custom-bg bg-cover">
+    <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 shadow-2xl shadow-black sm:rounded-lg backdrop-blur-3xl border-slate-500 border-4">
                 <div class="max-w-xl">
