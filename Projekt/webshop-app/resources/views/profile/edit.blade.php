d3bbefc849893f23ff8378c731e7126759bd4acd
BorsodiIstvan <borsodiistvan639@gmail.com>
2024-11-09 23:11:16 +0100

    Updated Projekt - Create frontend for edit-product, add-product
    and appearance changes

--- Projekt/webshop-app/resources/views/profile/edit.blade.php
+++ Projekt/webshop-app/resources/views/profile/edit.blade.php
@@ -8,19 +8,19 @@
     <div class="py-12">
         <div class="mx-5">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 shadow-2xl shadow-black sm:rounded-lg backdrop-blur-3xl border-slate-500 border-4">
+            <div class="p-4 sm:p-8 shadow-2xl shadow-black rounded-lg backdrop-blur-3xl border-slate-500 border-4">
                 <div class="max-w-xl">
                     @include('profile.partials.update-profile-information-form')
                 </div>
             </div>
 
-            <div class="p-4 sm:p-8 shadow-2xl shadow-black sm:rounded-lg backdrop-blur-3xl border-slate-500 border-4">
+            <div class="p-4 sm:p-8 shadow-2xl shadow-black rounded-lg backdrop-blur-3xl border-slate-500 border-4">
                 <div class="max-w-xl">
                     @include('profile.partials.update-password-form')
                 </div>
             </div>
 
-            <div class="p-4 sm:p-8 shadow-2xl shadow-black sm:rounded-lg backdrop-blur-3xl border-slate-500 border-4">
+            <div class="p-4 sm:p-8 shadow-2xl shadow-black rounded-lg backdrop-blur-3xl border-slate-500 border-4">
                 <div class="max-w-xl">
                     @include('profile.partials.delete-user-form')
                 </div>
